041ab7b7395fe152712564b655feddade27ee581
Ly-65IT5 Nguyễn Cẩm <ly127265@huce.edu.vn>
2024-12-02 11:57:49 +0700

    Sua popup bill

--- Group 5/AC_Tables.Designer.cs
+++ Group 5/AC_Tables.Designer.cs
@@ -32,11 +32,12 @@
             // 
             // AC_Tables
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.ClientSize = new System.Drawing.Size(790, 600);
+            this.ClientSize = new System.Drawing.Size(601, 482);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "AC_Tables";
             this.Text = "Form1";
             this.ResumeLayout(false);
