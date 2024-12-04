f7d7dc81621d154394599a4ec8be4634835d2b19
Cop221202 <luuquocgia12@gmail.com>
2024-12-04 17:24:15 +0700

    ...

--- Group 5/AC_AddtoBill.Designer.cs
+++ Group 5/AC_AddtoBill.Designer.cs
@@ -28,22 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imgfood = new System.Windows.Forms.PictureBox();
             this.exitbtn = new System.Windows.Forms.Button();
             this.lessbtn = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.morebtn = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.bill_here = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.Label();
+            this.txt_price = new System.Windows.Forms.Label();
+            this.txt_note = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_description = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgfood)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // imgfood
             // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.imgfood.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.imgfood.Location = new System.Drawing.Point(0, 0);
+            this.imgfood.Margin = new System.Windows.Forms.Padding(0);
+            this.imgfood.Name = "imgfood";
+            this.imgfood.Size = new System.Drawing.Size(300, 300);
+            this.imgfood.TabIndex = 0;
+            this.imgfood.TabStop = false;
             // 
             // exitbtn
             // 
@@ -68,7 +76,7 @@
             this.lessbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lessbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lessbtn.ForeColor = System.Drawing.Color.Maroon;
-            this.lessbtn.Location = new System.Drawing.Point(49, 378);
+            this.lessbtn.Location = new System.Drawing.Point(69, 365);
             this.lessbtn.Name = "lessbtn";
             this.lessbtn.Size = new System.Drawing.Size(40, 40);
             this.lessbtn.TabIndex = 3;
@@ -79,7 +87,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(112, 378);
+            this.label1.Location = new System.Drawing.Point(124, 365);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(37, 40);
             this.label1.TabIndex = 5;
@@ -91,12 +99,79 @@
             this.morebtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.morebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.morebtn.ForeColor = System.Drawing.Color.Maroon;
-            this.morebtn.Location = new System.Drawing.Point(189, 378);
+            this.morebtn.Location = new System.Drawing.Point(176, 365);
             this.morebtn.Name = "morebtn";
             this.morebtn.Size = new System.Drawing.Size(40, 40);
             this.morebtn.TabIndex = 6;
             this.morebtn.Text = "+";
             this.morebtn.UseVisualStyleBackColor = true;
+            // 
+            // bill_here
+            // 
+            this.bill_here.FormattingEnabled = true;
+            this.bill_here.Location = new System.Drawing.Point(453, 16);
+            this.bill_here.Name = "bill_here";
+            this.bill_here.Size = new System.Drawing.Size(121, 28);
+            this.bill_here.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(315, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 29);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Add to bill :";
+            // 
+            // txt_name
+            // 
+            this.txt_name.AutoSize = true;
+            this.txt_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_name.Location = new System.Drawing.Point(325, 55);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(98, 40);
+            this.txt_name.TabIndex = 9;
+            this.txt_name.Text = "label";
+            // 
+            // txt_price
+            // 
+            this.txt_price.AutoSize = true;
+            this.txt_price.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_price.Location = new System.Drawing.Point(643, 129);
+            this.txt_price.Name = "txt_price";
+            this.txt_price.Size = new System.Drawing.Size(69, 29);
+            this.txt_price.TabIndex = 11;
+            this.txt_price.Text = "Price";
+            // 
+            // txt_note
+            // 
+            this.txt_note.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txt_note.Location = new System.Drawing.Point(332, 232);
+            this.txt_note.Margin = new System.Windows.Forms.Padding(0);
+            this.txt_note.Name = "txt_note";
+            this.txt_note.Size = new System.Drawing.Size(380, 68);
+            this.txt_note.TabIndex = 12;
+            this.txt_note.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(303, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 32);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Note :";
+            // 
+            // txt_description
+            // 
+            this.txt_description.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_description.Location = new System.Drawing.Point(332, 98);
+            this.txt_description.Multiline = true;
+            this.txt_description.Name = "txt_description";
+            this.txt_description.Size = new System.Drawing.Size(277, 90);
+            this.txt_description.TabIndex = 14;
             // 
             // AC_AddtoBill
             // 
@@ -104,16 +179,24 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_description);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_note);
+            this.Controls.Add(this.txt_price);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bill_here);
             this.Controls.Add(this.morebtn);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lessbtn);
             this.Controls.Add(this.exitbtn);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.imgfood);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "AC_AddtoBill";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Load += new System.EventHandler(this.AC_AddtoBill_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgfood)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -121,10 +204,17 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox imgfood;
         private System.Windows.Forms.Button exitbtn;
         private System.Windows.Forms.Button lessbtn;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button morebtn;
+        private System.Windows.Forms.ComboBox bill_here;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label txt_name;
+        private System.Windows.Forms.Label txt_price;
+        private System.Windows.Forms.RichTextBox txt_note;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_description;
     }
 }
--- Group 5/AC_AddtoBill.cs
+++ Group 5/AC_AddtoBill.cs
@@ -8,33 +8,86 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static Group_5.AC_Menus;
+using Group_5.Model;
+using System.IO;
 
 namespace Group_5
 {
     public partial class AC_AddtoBill : Form
     {
-        public AC_AddtoBill(int idfood, int idorder)
+        int id_bill;
+        public AC_AddtoBill(int idfood, int idorder, int id_bill)
         {
             InitializeComponent();
 
 
             RoundedButton addtobillbtn = new RoundedButton();
-            addtobillbtn.Size = new Size(150, 50);  // Kích thước của nút
-            addtobillbtn.Location = new Point(300, 200);  // Vị trí của nút
+            addtobillbtn.Size = new Size(150, 30); // Kích thước của nút
+            addtobillbtn.Location = new Point(320, 230);  // Vị trí của nút
             addtobillbtn.Text = "Add to Bill";  // Văn bản của nút
-            addtobillbtn.BackColor = Color.IndianRed;  // Màu nền của nút
-            addtobillbtn.ForeColor = Color.White;  // Màu chữ
-            addtobillbtn.BorderRadius = 20;  // Bán kính bo tròn
-
-            // Cài đặt không có viền
+            addtobillbtn.Font = new Font("Times New Roman", 16, FontStyle.Bold);
+            addtobillbtn.BackColor = Color.FromArgb(192, 255, 192);
+            addtobillbtn.ForeColor = Color.IndianRed;
+            addtobillbtn.BorderRadius = 20;
             addtobillbtn.FlatStyle = FlatStyle.Flat;
             addtobillbtn.FlatAppearance.BorderSize = 0;  // Không có viền
+            addtobillbtn.TextAlign = ContentAlignment.MiddleCenter;
 
             // Xử lý sự kiện click
             addtobillbtn.Click += new EventHandler(addtobillbtn_Click);
 
-            // Thêm nút vào form
+            Menui wy = GetMenuItem(idfood);
+
+            txt_name.Text = wy.Name;
+            txt_description.Text = wy.Description;
+            txt_price.Text = "- " + wy.Price + " VND";
+
+            AC_Menus me = new AC_Menus();
+            imgfood.SizeMode = PictureBoxSizeMode.Zoom;
+            me.LoadImageFromUrl(wy.ImageCover, imgfood); // load anh
+
             this.Controls.Add(addtobillbtn);
+            this.id_bill = id_bill;
+        }
+
+        private Menui GetMenuItem(int idfood)
+        {
+            using (var context = new DataClasses1DataContext())
+            {
+                try
+                {
+                    var menuItem = context.Menus
+                        .Where(item => item.ID == idfood)  // Lọc theo ID món ăn
+                        .Select(item => new
+                        {
+                            item.ID,
+                            item.Name,
+                            item.Price,
+                            item.Kind,
+                            item.Description,
+                            item.Imagecover
+                        })
+                        .FirstOrDefault(); // Lấy kết quả đầu tiên nếu có
+
+                    Menui wy = new Menui
+                    {
+                        Id = menuItem.ID,
+                        Name = menuItem.Name,
+                        Price = menuItem.Price.ToString(),  // Nếu Price là int, chuyển sang string
+                        Kind = menuItem.Kind,
+                        Description = menuItem.Description,
+                        ImageCover = menuItem.Imagecover
+                    };
+
+                    return wy;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Đã xảy ra lỗi: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return null;
+                }
+            }
         }
 
         private void addtobillbtn_Click(object sender, EventArgs e)
@@ -48,6 +101,29 @@ namespace Group_5
         private void exitbtn_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void AC_AddtoBill_Load(object sender, EventArgs e)
+        {
+            bill_here.DataSource = null;
+            using (var context = new DataClasses1DataContext())
+            {
+                try
+                {
+                    DateTime today = DateTime.Today;
+
+                    var orders = context.Orders
+                        .Where(order => order.Status == 0 && order.Time.Date == today)
+                        .Select(order => new { order.ID, order.Name })  // Chọn các cột cần thiết
+                        .ToList();
+
+                    bill_here.DataSource = orders;
+                    bill_here.DisplayMember = "Name"; // Hiển thị tên đơn hàng
+                    bill_here.ValueMember = "ID";    // Giá trị của mỗi mục là ID của đơn hàng
+                }
+                catch (Exception ex) { MessageBox.Show("Error loading data: " + ex.Message); }
+            }
+            if (id_bill != null) bill_here.SelectedValue = id_bill;
         }
     }
 
